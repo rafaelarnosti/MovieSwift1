754474229ad77d43b6d4d202b0cd712f5fb1a173
riveregg <rlm.arnosti@gmail.com>
2017-09-27 21:09:13 -0300

    Rafael

--- MoviesLib/MoviesLib/SettingsViewController.swift
+++ MoviesLib/MoviesLib/SettingsViewController.swift
@@ -13,11 +13,37 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var scColors: UISegmentedControl!
     @IBOutlet weak var swAutoPlay: UISwitch!
     @IBOutlet weak var tfGenre: UITextField!
- 
+    
+    var pickerView: UIPickerView!
+    let genres = ["Ação","Comédia","Horror","Porn","Suspense"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerView = UIPickerView()
+        pickerView.backgroundColor = .white
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        tfGenre.inputView = pickerView
+        tfGenre.delegate = self
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 44))
+        let okButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        toolbar.items = [cancelButton,spaceButton,okButton]
+        
+        tfGenre.inputAccessoryView = toolbar
 
+    }
+    
+    func cancel(){
+        tfGenre.resignFirstResponder()
+    }
+    func done(){
+        tfGenre.text = genres[pickerView.selectedRow(inComponent: 0)]
+        UserDefaults.standard.set(tfGenre.text!, forKey: "genre")
+        cancel()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -41,4 +67,21 @@ class SettingsViewController: UIViewController {
     }
     
     
+}
+
+extension SettingsViewController: UIPickerViewDelegate,UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return genres.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return genres[row]
+    }
+}
+extension SettingsViewController: UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false
+    }
 }
